da267f693462a2f141411f16407b5b537d8413d9
Samarth Kamble <kamblesamarth04@gmail.com>
2025-07-19 20:59:01 +0530

    feat: implement the hero section, testimonials, pricing-card

--- app/site/page.tsx
+++ app/site/page.tsx
@@ -1,14 +1,7 @@
-import { Button } from "@/components/ui/button";
 import React from "react";
 
 const RootPage = () => {
-  return (
-    <div>
-      <h1>Welcome to the Root Page</h1>
-      <p>This is the main entry point of the application.</p>
-      <Button>Click Me</Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default RootPage;
