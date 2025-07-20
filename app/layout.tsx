3b100518e359e0809217a928c8508d9d87161dab
Samarth Kamble <kamblesamarth04@gmail.com>
2025-07-20 15:18:04 +0530

    Implement the font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,17 @@
 import type { Metadata } from "next";
-import { DM_Mono, DM_Sans } from "next/font/google";
+import { IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/ThemeProvider";
-import AuthProvider from "@/components/providers/AuthProvider";
 
 export const metadata: Metadata = {
   title: "Plura",
   description: "All in one Agency Solution",
 };
 
-const fontSans = DM_Sans({ subsets: ["latin"], variable: "--font-dm-sans" });
-const fontMono = DM_Mono({
+const ibmPlexSans = IBM_Plex_Sans({
   subsets: ["latin"],
-  variable: "--font-dm-mono",
-  weight: "400",
+  variable: "--font-ibm-plex-sans",
 });
 
 export default function RootLayout({
@@ -27,8 +24,7 @@ export default function RootLayout({
       <body
         className={cn(
           "relative h-full font-sans antialiased min-h-screen",
-          fontSans.variable,
-          fontMono.variable
+          ibmPlexSans.variable
         )}
       >
         <ThemeProvider
